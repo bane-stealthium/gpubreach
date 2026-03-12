d6982af3fa3476e6e26fbde2b8e58cd7a1ac4f4c
Root User <shaopeng.lin@mail.utoronto.ca>
2026-03-12 12:00:30 -0400

    Added cuPQC exploit initial file

--- run_auto_artifacts.sh
+++ run_auto_artifacts.sh
@@ -13,13 +13,23 @@ cd src/
 cmake -S . -B out
 cd out && make
 
+cd ../..
+
+if [ ! -e "./cupqc_exploit/cupqc-sdk-0.4.0-x86_64" ]; then
+    cd ./cupqc_exploit
+    wget https://developer.nvidia.com/downloads/compute/cupqc/downloads/secure/cupqc-sdk-0.4.0-x86_64.tar.gz
+    tar -xvzf cupqc-sdk-0.4.0-x86_64.tar.gz
+    rm cupqc-sdk-0.4.0-x86_64.tar.gz
+    cd ..
+fi
+
 echo "-------------------------------------------"
 echo ""
 echo "###########################################"
 echo "[INFO] 3. Running Artifacts"
 echo "###########################################"
 
-bash run_fig5.sh
-bash run_fig7.sh
-bash run_fig8.sh
-bash run_fig10.sh
+# bash run_fig5.sh
+# bash run_fig7.sh
+# bash run_fig8.sh
+# bash run_fig10.sh
