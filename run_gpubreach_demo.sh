3a1e16c102dba89b8bcf38afb9758da818c40009
Shaopeng (Chris) Lin <shaopeng.lin@mail.utoronto.ca>
2026-03-17 19:54:16 -0400

    Update run_gpubreach_demo.sh

--- run_gpubreach_demo.sh
+++ run_gpubreach_demo.sh
@@ -9,7 +9,7 @@ cd data_scripts/gpubreach_demo
 nvcc sample_app.cu -ccbin g++-10 -O3 -Xcicc -O0 -Xptxas -O3 --generate-line-info -gencode=arch=compute_80,code=sm_80 -o app
 cd ../../
 mkdir -p results/gpubreach_demo
-> ./results/gpubreach_demo/app_out.out
+> ./results/gpubreach_demo/app.out
 python3 gpubreach.py app_demo --n_step1 24109 --n_step3 24070 -t 0.2 -s 15
 sleep 5
 
